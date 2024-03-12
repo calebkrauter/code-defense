051d1edc6da70e3958ea8d6a62ac127e084c8f57
Caleb Krauter <ghostMe66+github@proton.me>
2024-03-12 01:50:58 -0700

    Update to code defense project. Final submission.
    Add logs for bad input.
    Add pdfs for test cases.
    Add assignment details.

--- code_defense.py
+++ code_defense.py
@@ -11,7 +11,10 @@ def mismatched_input(input, regex):
     pattern = re.compile(regex)
     return not re.match(pattern, input)
 
-def log_
+def log_problem(message):
+    file = open("resources/log.txt", "a")
+    file.write("\n" + message)
+    file.close()
 
 def prompt_for_name(first_or_last):
     print("Please provide your " + first_or_last + " name.")
@@ -19,8 +22,9 @@ def prompt_for_name(first_or_last):
 
     name = input()
 
-    if (mismatched_input(name, "^[a-zA-Z0-9]{1,50}$")):
+    if (mismatched_input(name, "^[a-zA-Z]{1,50}$")):
         print("Not a valid name. Please try again.")
+        log_problem("Invalid name entered: " + name)
         return prompt_for_name(first_or_last)
     
     return name
@@ -35,11 +39,13 @@ def prompt_for_int():
         value = int(input("Please provide a 4 byte integer."))
     except:
         print("Not an integer. Please try again.")
-        prompt_for_int()
+        log_problem("Invalid integer: " + str(value))
+        return prompt_for_int()
 
     if (value < min or value > max):
         print("Integer was too many bytes. Please try again.")
-        prompt_for_int()
+        log_problem("Integer out of bounds: " + str(value))
+        return prompt_for_int()
 
     return value
 
@@ -52,6 +58,7 @@ def prompt_for_input_file_name():
 
     if (mismatched_input(name, "^[a-zA-Z0-9]{1,50}.txt$")):
         print("Not a valid file name. Please try again.")
+        log_problem("Invalid input file name: " + name)
         return prompt_for_input_file_name()
     
     return name
@@ -65,6 +72,7 @@ def prompt_for_output_file_name(input_file_name):
 
     if (mismatched_input(name, "^[a-zA-Z0-9]{1,50}.txt$") or name == input_file_name):
         print("Not a valid file name. Please try again.")
+        log_problem("Invalid output file name: " + name)
         return prompt_for_output_file_name(input_file_name)
     
     return name
@@ -76,6 +84,8 @@ def prompt_for_password(message):
     pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[\\.,!\\?'\";:-])(?!.*[a-z]{4,}).{10,}$"
     if re.search(pattern, password):
         return password
+    
+    log_problem("Invalid password: " + password)
     return prompt_for_password("Invalid password. Please try again.")
 
 def hash_password(password, salt):
@@ -102,6 +112,7 @@ def compare_password():
     password = prompt_for_password("Please re-enter your password.")
     
     if not os.path.exists("./resources/password.txt"):
+        log_problem("Password file not found. Reprompting.")
         return get_passwords()
 
     file = open("./resources/password.txt", "r")
@@ -110,9 +121,12 @@ def compare_password():
     file.close()
 
     if not salt or not hash:
+        log_problem("Salt or hash not found. Reprompting.")
         return get_passwords()
     
+    hash2 = hash_password(password, salt)
     if hash != hash_password(password, salt):
+        log_problem("Password hashes (" + hash + ", " + hash2 + ") do not match.")
         compare_password()
 
 def get_passwords():
@@ -124,6 +138,7 @@ def write_to_file(first_name, last_name, input_file_name, output_file_name, inte
     prod = integer1 * integer2
     
     while (not os.path.exists(input_file_name)):
+        log_problem("Input file not found. Reprompting.")
         input_file_name = prompt_for_input_file_name()
 
     fileData = ""
---
+++ krauterclaarhinthorne/readme.txt
@@ -0,0 +1,31 @@
+code-defense
+ 
+Team Members
+- [Nathan Hinthorne]
+- [Caleb Krauter]
+- [Trae Claar]
+
+Shortcomings
+- We were able to complete the project without any shortcomings that we are aware of.
+
+Cases we explicitly tested against
+- No input
+- Strings when integers were expected
+- Integers when strings were expected
+- Out of bounds integers
+- Strings that were too long
+- Strings that were too short
+- Extensions other than .txt
+
+Compilation Instructions
+Run the CodeDefense.py file with the following command:
+bash
+python3 CodeDefense.py
+
+
+Run the CodeDefense.java file with the following command:
+bash
+javac CodeDefense.java
+java CodeDefense
+
+
